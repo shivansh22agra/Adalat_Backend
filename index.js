78bf4ec8828430e4aa3a0f62ed8e36244fa3ac0c
shivansh22agra <agrawalshivansh22@gmail.com>
2023-09-21 19:01:52 +0530

    chat update

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-import express from "express";
 import { config } from "dotenv";
-import dbConnect from "./dbConnect.js";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { dirname } from "path";
 import userRoutes from "./controllers/user.js";
+import dbConnect from "./dbConnect.js";
 import lawyerRoute from "./routes/lawyer.js";
+import { fileURLToPath } from "url";
+
+import onSocket from "./socket.js";
 
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const httpServer = createServer(app);
 config();
 dbConnect();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
 
 app.use("/", userRoutes);
-app.use("/",lawyerRoute);
+app.use("/", lawyerRoute);
+
+const io = new Server(httpServer);
+onSocket(io);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+httpServer.listen(3000, () => console.log(`Listening on port ${3000}...`));
